e64bc2df2362b176c53f394c094d0ca598b15c7d
Martin Dilorenzo <dimartechnologies@gmail.com>
2021-02-05 19:47:39 -0300

    Modelo Clientes

---
+++ models/Cliente.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const ClientesSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    apellido: {
+        type: String,
+        required: true,
+        trim: true
+    },fechaNac: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    telefono: {
+        type: String,
+        trim: true
+    },
+    medicacion: {
+        type: String,
+        trim: true
+    },
+    productos: {
+        type: String,
+        trim: true
+    },
+    uñas: {
+        type: String,
+        trim: true
+    },
+    cabello: {
+        type: String,
+        trim: true
+    },
+    cueroCab: {
+        type: String,
+        trim: true
+    },
+    creado: {
+        type: Date,
+        default: Date.now()
+    }/*,
+    //Esto por el momento no es necesario,
+    //Se deja a modo de ejemplo
+    usuarioAlta: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Usuario'
+    }
+    */
+
+});
+
+
+module.exports = mongoose.model('Cliente', ClientesSchema);
